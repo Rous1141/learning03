cedc394ab761145f3168185d5339c8aef5c1b13e
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-01-04 21:02:40 +0700

    commit 2

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 
 export default function Page() {
   return (
-    <div className='page'>
+    <div className='page'> 
       <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
