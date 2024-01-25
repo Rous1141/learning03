7427248ac5e153ba7eb7c5183a03bd06c1cf024e
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-01-25 22:42:29 +0700

    nnnn

--- src/Components/UpdatePeople.jsx
+++ src/Components/UpdatePeople.jsx
@@ -8,12 +8,16 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import LoadingScreen from './LoadingScreen';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 //Have to keep the MUI copyrights for now
 function Copyright(props) {
@@ -28,21 +32,29 @@ function Copyright(props) {
         </Typography>
     );
 }
-
 // TODO remove, this demo shouldn't need to reset the theme.
 
+
 export default function UpdatePeople() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = (event) => {
+      event.preventDefault();
+      setOpen(true);
+    };
+  
+    const handleClose = (event) => {
+      event.preventDefault();
+      setOpen(false);
     };
 
     //Get The PUT API and update
-    const [name,Setname] = useState()
+    const [name,Setname] = useState();
+    const [age,Setage] = useState();
+    const [job,Setjob] = useState();
+    const [image,Setimage] = useState();
+     
+
     const [person,SetPerson] = useState([]);
     const {id} = useParams();
     const URL = "https://peopleapi1141.azurewebsites.net/api/people/" + id;
@@ -56,17 +68,27 @@ export default function UpdatePeople() {
     if(person.id==null){
         return <LoadingScreen/>
     }
-    const HandleOnChange = () =>{
-
+      
+    const handleSubmit = (event) =>{
+        axios.put(URL,{
+            id: person.id,
+            name: name,
+            age: age,
+            job: job,
+            image: image
+        })
+        .then(response => {console.log("Begin PUT method: ",response.data);})
+        .catch(error => console.log(error))
+        event.preventDefault(); 
+        handleClose(event);
     }
-
-    const HandleClick = () => {
-        
-    }
+    
+    //To get a constant update of an variable, use function event => SetState(event.target.value)
     return (
         <div className='page'>
             <Container className='container' component="main" maxWidth="xs">
                 <CssBaseline />
+                
                 <Box
                     sx={{
                         marginTop: 8,
@@ -76,26 +98,28 @@ export default function UpdatePeople() {
                     }}
                 >
                     <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
+                        <img alt='character' style={{objectFit:"fill"}} src = {person.image} ></img>
                     </Avatar>
                     <Typography component="h1" variant="h5">
                         Update The Character
                     </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                    <Box component="form" noValidate onSubmit={event => handleClickOpen(event)} sx={{ mt: 3 }}>
                         <Grid container spacing={2}>
                             <Grid item xs={12} sm={6}>
-                                <TextField
+                                <TextField 
+                                    onChange={event => Setname(event.target.value)}
+                                    defaultValue = {person.name}
                                     name="fullName"
                                     required
                                     fullWidth
                                     id="fullName"
                                     label="Full Name"
-                                    defaultValue = {person.name}
                                 />
                             </Grid>
                             <Grid item xs={12} sm={6}>
                                 <TextField
-                                defaultValue = {person.age}
+                                    onChange={event => Setage(event.target.value)}
+                                    defaultValue = {person.age}
                                     required
                                     fullWidth
                                     id="age"
@@ -105,7 +129,8 @@ export default function UpdatePeople() {
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
-                                defaultValue = {person.job}
+                                    onChange={event => Setjob(event.target.value)}
+                                    defaultValue = {person.job}
                                     required
                                     fullWidth
                                     id="job"
@@ -116,7 +141,8 @@ export default function UpdatePeople() {
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
-                                defaultValue = {person.image}
+                                    onChange={event => Setimage(event.target.value)}
+                                    defaultValue = {person.image}
                                     required
                                     fullWidth
                                     name="imageurl"
@@ -147,12 +173,36 @@ export default function UpdatePeople() {
                         </Grid>
                     </Box>
                 </Box>
+                
                 <Copyright sx={{ mt: 5 }} />
             </Container>
 
-            <p>{person.name}</p>
-            <p>{person.age}</p>
-            <p>{person.job}</p>
+            <p>{name}</p>
+            <p>{age}</p>
+            <p>{job}</p>
+
+            <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={event => handleSubmit(event)} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
         </div>
     );
 }
