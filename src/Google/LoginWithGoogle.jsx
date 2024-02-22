08642ad16f91dc644272606d71818a8e82d1f32a
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-02-22 21:22:57 +0700

    google Remaster

--- src/Google/LoginWithGoogle.jsx
+++ src/Google/LoginWithGoogle.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import {useGoogleLogin} from '@react-oauth/google'
 import GoogleIcon from '@mui/icons-material/Google'; 
 import Button from '@mui/material/Button';
+import  axios  from 'axios';
 export default function LoginWithGoogle() {
     // const googleResponse = (response) =>{
     //     const { profileObj } = response
     //     console.log(`Welcome Back ${profileObj.name}!`)
     // }
+    const googleAPI = 'https://www.googleapis.com/oauth2/v3/userinfo'
     const onClick = useGoogleLogin({
-        onSuccess: response => {console.log(response)},
+        onSuccess: response => {console.log(response);
+           const token = (response.access_token);
+            //Using Axios to fetch API from Google
+           const userInfo = axios
+                            .get(googleAPI,{headers:{Authorization:`Bearer ${token}`}})
+                            .catch(error => console.log(error))
+            console.log(userInfo)
+        },
         onError: error => {console.log(error)}
     })
   return (
