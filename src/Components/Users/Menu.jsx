8052352492f928d2fca0b5946e0b9ff5568605bb
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-02-26 19:46:24 +0700

    Last Update

--- src/Components/Users/Menu.jsx
+++ src/Components/Users/Menu.jsx
@@ -4,34 +4,29 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
 import { ThemeContext } from '../Themes/ThemeProvider.tsx';
-
+import CustomizedDrawer from '../StyledMUI/CustomizedDrawer.jsx';
+import AppLogo from '../StyledMUI/AppLogo.jsx';
+import ExpandingSearchBar from '../StyledMUI/CustomizedSearchBar.jsx'
 
 export default function Menu() {
   const { theme, toggleTheme, dark } = useContext(ThemeContext);
   return (
     <div>
-      <Box sx={{ flexGrow: 1 ,boxShadow: '50px'}}>
-        <AppBar sx={{transition:theme.transition,color:theme.color,backgroundColor: theme.backgroundColor }} position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography color="inherit" variant="h5" component="div" sx={{flexGrow: 1, fontFamily:theme.fontFamily}}>
-              Gensokyo
-            </Typography>
-            <Button color="inherit"><Link to={"/"}>Home</Link></Button>
-            <Button color="inherit"><Link to={"create"}>Create</Link></Button>
-            <Button color="inherit" onClick={toggleTheme}>{dark?"Dark":"Light"}</Button>
+      <Box sx={{ flexGrow: 1, boxShadow: '50px' }}>
+        <AppBar sx={{ transition: theme.transition, color: theme.color, backgroundColor: theme.backgroundColor }} position="static">
+          <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <CustomizedDrawer />
+              <AppLogo />
+              <ExpandingSearchBar />
+            </Box>
+            <Box>
+              <Button color="inherit"><Link to={"/"}>Home</Link></Button>
+              <Button color="inherit"><Link to={"create"}>Create</Link></Button>
+              <Button color="inherit" onClick={toggleTheme}>{dark ? "Dark" : "Light"}</Button>
+            </Box>
           </Toolbar>
         </AppBar>
       </Box>
